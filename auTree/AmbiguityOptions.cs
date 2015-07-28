4e283091117a9b0711a2c75f57bc2a3d462182f9
Dmitrij <Daemon2017@gmail.com>
2015-07-28 19:19:44 +0300

    0.3.2

--- auTree/AmbiguityOptions.cs
+++ auTree/AmbiguityOptions.cs
@@ -7,12 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace auTree
 {
     public partial class AmbiguityOptions : Form
     {
-        public TextBox[] dis = new TextBox[5];
+        public TextBox[] ambiguityDistance = new TextBox[5];
+        public string[] masDis = new String[5];
 
         public AmbiguityOptions()
         {
@@ -21,17 +23,56 @@ namespace auTree
 
         private void button1_Click(object sender, EventArgs e)
         {
+            for (int j = 0; j < 5; j++)
+            {
+                masDis[j] = ambiguityDistance[j].Text;
+            }
+
+            saveMatrix();
+
             Close();
         }
 
         private void AmbiguityOptions_Load(object sender, EventArgs e)
         {
+            drawTextBoxes();
+
+            loadMatrix();
+        }
+
+        void drawTextBoxes()
+        {
             for (int i = 0; i < 5; i++)
             {
-                dis[i] = new TextBox();
-                dis[i].Location = new Point(148, 25 + i * 26);
-                dis[i].Size = new System.Drawing.Size(24, 20);
-                this.Controls.Add(dis[i]);
+                ambiguityDistance[i] = new TextBox();
+                ambiguityDistance[i].Location = new Point(148, 25 + i * 26);
+                ambiguityDistance[i].Size = new System.Drawing.Size(24, 20);
+                this.Controls.Add(ambiguityDistance[i]);
+            }
+        }
+
+        void loadMatrix()
+        {
+            string[] ambiguityEdge = File.ReadAllLines("AmbiguityOptions.cfg");
+            double[] edge = new double[5];
+
+            for (int i = 0; i < ambiguityEdge.Length; i++)
+            {
+                if (Double.TryParse(ambiguityEdge[i], out edge[i])) ;
+                ambiguityDistance[i].Text = edge[i].ToString();
+            }
+        }
+
+        void saveMatrix()
+        {
+            using (StreamWriter gg = new StreamWriter(@"AmbiguityOptions.cfg"))
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    gg.Write(masDis[i]);
+                    gg.Write("\r\n");
+                }
+                gg.Close();
             }
         }
     }
